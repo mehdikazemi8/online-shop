b119fee0006e6b8cbf639e0b595172baf7eb1a66
MeHdi <mehdi.kazemi8@gmail.com>
2017-10-28 14:27:48 +0330

    share special offer to telegram including its link

--- app/src/main/java/com/veevapp/customer/ui/singlespecialoffer/SingleSpecialOfferController.java
+++ app/src/main/java/com/veevapp/customer/ui/singlespecialoffer/SingleSpecialOfferController.java
@@ -40,7 +40,7 @@ import butterknife.OnClick;
 
 public class SingleSpecialOfferController
         extends HeaderController
-        implements SingleSpecialOfferContract.View,AppTickHandler.OnTickListener {
+        implements SingleSpecialOfferContract.View, AppTickHandler.OnTickListener {
 
     @BindView(R.id.iv_photo)
     ImageView ivPhoto;
@@ -68,6 +68,29 @@ public class SingleSpecialOfferController
     @BindView(R.id.tv_telegram)
     TextView tvTelegram;
 
+    @OnClick(R.id.share_button_container)
+    public void shareSpecialOffer() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        String recipeShareLink = "http://www.veevapp.com/specialOffer/asoeunthaeosut/";
+        sendIntent.putExtra(Intent.EXTRA_TEXT,
+                String.format(
+                        getActivity().getString(R.string.template_share_recipe),
+                        specialOffer.getProduct().getName(),
+                        specialOffer.getSuggestedPrice(),
+                        recipeShareLink
+                )
+        );
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("org.telegram.messenger");
+
+        try {
+            startActivity(sendIntent);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(recipeShareLink)));
+        }
+    }
+
     private SpecialOffer specialOffer;
     private int fromPosition;
     private SingleSpecialOfferContract.Presenter presenter;
@@ -82,22 +105,19 @@ public class SingleSpecialOfferController
     private void init() {
         tvPreviousPrice.setPaintFlags(tvPreviousPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
-        if(specialOffer==null)return;
+        if (specialOffer == null) return;
 
 
         headerTitle.setText(getResources().getString(R.string.special_offer));
         tvPrice.setText(String.format(getActivity().getString(R.string.template_price), String.valueOf(specialOffer.getSuggestedPrice())));
         tvPreviousPrice.setText(String.format(getActivity().getString(R.string.template_previous_price), String.valueOf(specialOffer.getProduct().getPrice())));
         ViewCompat.setTransitionName(ivPhoto, getResources().getString(R.string.transition_special_offer_photo_index, fromPosition));
-        tvProductName.setText(getActivity().getString(R.string.template_product_name,specialOffer.getProduct().getName()));
+        tvProductName.setText(getActivity().getString(R.string.template_product_name, specialOffer.getProduct().getName()));
         setPhoto(specialOffer.getProduct());
 
 
-
-
-
         Seller seller = specialOffer.getSeller();
-        if(seller!=null) {
+        if (seller != null) {
 
             String sellerName;
             if (!TextUtils.isEmpty(seller.getName()) && !TextUtils.isEmpty(seller.getFamily()))
@@ -125,8 +145,8 @@ public class SingleSpecialOfferController
             if (latLngs != null && latLngs.size() == 2) {
                 tvShopAddress.setText(tvShopAddress.getText() + " " + getActivity().getString(R.string.show_on_map));
                 SpannableString ss = new SpannableString(tvShopAddress.getText());
-                TextSpannableHandler.setColor(ss,getActivity().getString(R.string.show_on_map),
-                        ContextCompat.getColor(getActivity(),R.color.blue_link));
+                TextSpannableHandler.setColor(ss, getActivity().getString(R.string.show_on_map),
+                        ContextCompat.getColor(getActivity(), R.color.blue_link));
                 tvShopAddress.setText(ss);
             }
 
@@ -143,8 +163,8 @@ public class SingleSpecialOfferController
     void setPhoto(Product product) {
         if (product != null && product.getPhotoURLs() != null && product.getPhotoURLs().size() > 0) {
             ImageHandler.getInstance(getActivity())
-                    .loadImage(product.getPhotoURLs().get(0),ivPhoto,
-                            false,true,true,0);
+                    .loadImage(product.getPhotoURLs().get(0), ivPhoto,
+                            false, true, true, 0);
         }
     }
 
@@ -167,11 +187,11 @@ public class SingleSpecialOfferController
     }
 
     @OnClick(R.id.tv_shopAddress)
-    void onAddressClicked(){
+    void onAddressClicked() {
         double lat = specialOffer.getSeller().getLocation().get(0);
         double lon = specialOffer.getSeller().getLocation().get(1);
         getRouter().pushController(
-                RouterTransaction.with(ShowLocationController.newInstance(lat,lon,specialOffer.getSeller().getShopName()))
+                RouterTransaction.with(ShowLocationController.newInstance(lat, lon, specialOffer.getSeller().getShopName()))
                         .pushChangeHandler(new FadeChangeHandler())
                         .popChangeHandler(new FadeChangeHandler())
         );
@@ -210,9 +230,9 @@ public class SingleSpecialOfferController
         return inflater.inflate(R.layout.controller_single_special_offer, container, false);
     }
 
-    private void refreshTimer(){
+    private void refreshTimer() {
         AppHandler.RemainingTimeObject rto = AppHandler.getRemainingTime(specialOffer);
-        tvTimer.setText(AppHandler.getRemainingTimeStr(getActivity(),rto));
+        tvTimer.setText(AppHandler.getRemainingTimeStr(getActivity(), rto));
     }
 
     @Override
