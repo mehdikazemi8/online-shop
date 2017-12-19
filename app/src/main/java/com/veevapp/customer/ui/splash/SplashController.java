266195af44b9ce68b83f6328e4600007ef2a1355
MeHdi <mehdi.kazemi8@gmail.com>
2017-12-19 13:50:24 +0330

    add splash gif

--- app/src/main/java/com/veevapp/customer/ui/splash/SplashController.java
+++ app/src/main/java/com/veevapp/customer/ui/splash/SplashController.java
@@ -8,17 +8,26 @@ import android.view.ViewGroup;
 import com.bluelinelabs.conductor.RouterTransaction;
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler;
 import com.bluelinelabs.conductor.changehandler.VerticalChangeHandler;
-import com.veevapp.customer.controller.base.BaseController;
 import com.veevapp.customer.R;
+import com.veevapp.customer.controller.base.BaseController;
 import com.veevapp.customer.data.DataRepository;
 import com.veevapp.customer.data.local.PreferenceManager;
 import com.veevapp.customer.ui.entermobile.EnterMobileController;
 import com.veevapp.customer.ui.main.MainController;
 import com.veevapp.customer.ui.register.RegisterController;
 
+import butterknife.BindView;
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 public class SplashController extends BaseController implements SplashContract.View {
 
     private SplashContract.Presenter presenter;
+    private GifDrawable gifDrawable;
+
+    @BindView(R.id.splash_animation)
+    GifImageView splashAnimation;
+
 
     @Override
     protected View inflateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
@@ -26,10 +35,19 @@ public class SplashController extends BaseController implements SplashContract.V
     }
 
     @Override
-    protected void onAttach(@NonNull View view) {
-        super.onAttach(view);
+    protected void onViewBound(@NonNull View view) {
+        super.onViewBound(view);
+
         presenter = new SplashPresenter(DataRepository.getInstance(), PreferenceManager.getInstance(getActivity()), this);
-        presenter.start();
+        initGif();
+    }
+
+    private void initGif() {
+        gifDrawable = (GifDrawable) splashAnimation.getDrawable();
+        gifDrawable.addAnimationListener(loopNumber -> {
+            gifDrawable.stop();
+            presenter.start();
+        });
     }
 
     @Override
